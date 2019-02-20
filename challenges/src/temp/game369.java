acab8d80936acf9ffa060bad83abe63613811cc6
ParkHongSoon <ParkHongSoon@hs-pc>
2019-02-21 00:44:48 +0900

    test

---
+++ challenges/src/temp/game369.java
@@ -0,0 +1,48 @@
+package temp;
+
+public class game369 {
+
+
+	// 369 게임
+
+	public static void main (String arg[]) throws Exception{
+
+
+//		Scanner sc = new Scanner(System.in);
+
+//		int num = sc.nextInt();
+
+		int num = 100;
+
+//		System.out.println((int) Math.log10(100) + 1);
+
+//		System.out.println( Math.round(Math.pow(10,(int) Math.log10(100))));
+
+
+
+
+		String c = "";
+
+		for(int i = 1; i<=num; i++){
+
+			String a = String.valueOf(i);
+
+			for(int b = 0; b < a.length(); b++){
+				System.out.println(Character.getNumericValue(a.charAt(b)));
+			}
+
+		}
+
+
+
+
+	}
+
+
+}
+
+
+
+
+
+
